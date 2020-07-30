9af4868346746681e7e67baac8174f90f58aadfe
Tamas Kapolnasi <thomas.kapolnasi@gmail.com>
2020-07-30 10:25:57 +0200

    - add python venv

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,18 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
+from config import Config
+from forms import *
+
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route('/')
-def api_root():
-    return 'Welcome'
+def root(name=None):
+    return render_template('base.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 @app.route('/articles')
 def api_articles():
